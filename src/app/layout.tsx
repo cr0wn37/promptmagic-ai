5a152c49b16503f66ffa3eac5a777df8e5012d5a
Ishan Parab <ishan.parab.comp@gmail.com>
2025-09-26 12:41:51 +0530

    Added logo to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,29 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
    title: "PromptMagic",
  description:
-    "Organize, run, and save AI prompts with ease. Designed for professionals in fitness, HR, education, marketing, and more.",
+    "Clean, consistent, and tailored to your clients — every single time. Save hours of time, boost productivity, and scale your expertise. Designed for professionals in Fitness, HR, Sales, Marketing, Education and more.",
+    icons: {
+    icon: "/logo-icon.png",
+    shortcut: "/logo-icon.png",
+    apple: "/favicon-ico.png",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning >
+       <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "PromptMagic",
+              url: "https://thepromptmagic.com",
+              logo: "https://thepromptmagic.com/logo-icon.png",
+            }),
+          }}
+        />
       <body className={`${inter.className} bg-white text-gray-900 dark:bg-black dark:text-gray-100`}>
         {/* ThemeProvider from next-themes for light/dark mode */}
         {/* It must wrap the children to provide context */}
